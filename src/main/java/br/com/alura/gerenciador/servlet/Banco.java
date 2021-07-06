db630ec3908f0af334cf8f1f5a8a183de1e7f861
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-07-05 23:05:31 -0300

    Update Banco.java

--- src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -6,6 +6,18 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Google");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Alura");
+		Empresa empresa3 = new Empresa();
+		empresa3.setNome("Caelum");
+		lista.add(empresa);
+		lista.add(empresa2);
+		lista.add(empresa3);
+	}
 
 	public void adiciona(Empresa empresa) {
 		Banco.lista.add(empresa);
